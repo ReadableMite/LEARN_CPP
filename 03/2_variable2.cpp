47084b32cf47ffacca926aceff3c458a96f00458
David Dwi Wahyudi <daviddwiwahyudi626@gmail.com>
2026-05-22 21:22:14 +0700

    pindah alamat folder ke PROJECTS

---
+++ 03/2_variable2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Today we learn about Vaiables\n";
+	std::cout << "Variables are container for storing data values.\n";
+	// Kali ini kita akan belajar mengenai variable
+	// Variable adalah kontainer yang erfungsi untuk menyimpan nilai pada suatu data
+
+	std::cout << "\nValue in variables can be change\n";
+	std::cout << "If you assign a new value to an existing variable, it will overwrite the previous value\n";
+	// Nilai pada variables dapat diubah
+	// Jika kamu mencatat nilai baru ke variable yang sudah ada, maka nilai sebelumnya akan berubah ke nilai yang baru
+
+	std::cout << "\nChanging variable values: ex.\n";
+	std::cout << 
+}
